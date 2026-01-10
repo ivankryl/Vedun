fbe9a25ad76733a902e3af9aafa2f38911143f22
Ivan <alfaminer@mail.ru>
2026-01-10 12:04:11 -0800

    fixed controller ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,16 +4,11 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-@Post('login')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-
-//    @Controller('api/auth')
-//
-//    export class AuthController {
-//      constructor(private readonly authService: AuthService) {}
-//
-//      @Post('login')
-//      async login(@Body() dto: LoginDto) {
-//        return this.authService.login(dto);
-//      }
-//    }
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+}
