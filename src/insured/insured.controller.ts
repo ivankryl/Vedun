413b365f6862c6d26d5a479b2cb986d8d859ffec
Ivan <alfaminer@mail.ru>
2026-01-10 16:16:01 -0800

    fix contr1

--- src/insured/insured.controller.ts
+++ src/insured/insured.controller.ts
@@ -1,27 +1,10 @@
-import { Controller, Get, Post, Body, UseGuards, Req, ForbiddenException } from '@nestjs/common';
-import { InsuredService } from './insured.service';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-
 @UseGuards(JwtAuthGuard)
 @Controller('insured')
 export class InsuredController {
   constructor(private readonly insuredService: InsuredService) {}
 
   @Get()
-  list(@Req() req: any) {
-    const orgId = req?.user?.orgId;
-    if (!orgId) {
-      throw new ForbiddenException('User has no organization (orgId is missing)');
-    }
-    return this.insuredService.listForOrg(orgId);
-  }
-
-  @Post()
-  create(@Req() req: any, @Body() dto: any) {
-    const orgId = req?.user?.orgId;
-    if (!orgId) {
-      throw new ForbiddenException('User has no organization (orgId is missing)');
-    }
-    return this.insuredService.createForOrg(orgId, dto);
+  list() {
+    return this.insuredService.findAll();
   }
 }
