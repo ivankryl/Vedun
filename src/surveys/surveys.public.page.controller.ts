296bc0ed49b85c0c771ed1c9e12d6dd4b5425b27
Ivan <alfaminer@mail.ru>
2026-02-14 19:20:04 -0800

    fixed surv2

--- src/surveys/surveys.public.page.controller.ts
+++ src/surveys/surveys.public.page.controller.ts
@@ -20,18 +20,25 @@ export class SurveysPublicPageController {
       templateKey === 'large' ? 'v1_large.html' :
       'v1_small.html';
 
-    const filePath = path.join(process.cwd(), 'dist', 'surveys', 'templates', fileName);
+    // ✅ ВАЖНО: __dirname вместо process.cwd()
+    const filePath = path.join(__dirname, 'templates', fileName);
+
+    // ✅ Временный дебаг (можно убрать после)
+    console.log('[SURVEY PAGE] templateKey=', templateKey, 'fileName=', fileName);
+    console.log('[SURVEY PAGE] __dirname=', __dirname);
+    console.log('[SURVEY PAGE] filePath=', filePath);
+
     let html = await fs.readFile(filePath, 'utf-8');
 
     const apiBase = (process.env.PUBLIC_API_BASE_URL || '').replace(/\/$/, '');
-    
-      const replaceAll = (s: string, search: string, value: string) =>
-        s.split(search).join(value);
 
-      html = replaceAll(html, '__TOKEN__', token);
-      html = replaceAll(html, '__API_BASE__', apiBase);
-      html = replaceAll(html, '__SURVEY_TITLE__', link.survey?.title ?? 'Опрос');
-    
+    const replaceAll = (s: string, search: string, value: string) =>
+      s.split(search).join(value);
+
+    html = replaceAll(html, '__TOKEN__', token);
+    html = replaceAll(html, '__API_BASE__', apiBase);
+    html = replaceAll(html, '__SURVEY_TITLE__', link.survey?.title ?? 'Опрос');
+
     res.type('html');
     return res.send(html);
   }
